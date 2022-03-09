1f9bf22ddd4cb6e30597fde2d6a3ccb5abf05013
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-09 12:13:50 +0300

    Added a new functionality

--- src/Components/menudata.js
+++ src/Components/menudata.js
@@ -1,68 +1,68 @@
-import Chinesefoods from '../Images/chinese1.jpeg';
-import FamilyDinner from '../Images/dinnerspecial.jpg';
-import Indianfoods from '../Images/indian.jpg';
-import Koreanfoods from '../Images/korean1.jpeg';
-import Mexicanfoods from '../Images/mexican.jpg';
-import Special from '../Images/restrauntk.jpg';
-import Sea from '../Images/seefood2.jpg';
-import Wine from '../Images/wine6.jpg';
-import Dessert from '../Images/dessertspecial.jpeg';
+import chinesefoods from '../Images/chinese1.jpeg';
+import family from '../Images/dinnerspecial.jpg';
+import indian from '../Images/indian.jpg';
+import korean from '../Images/korean1.jpeg';
+import mexican from '../Images/mexican.jpg';
+import special from '../Images/restrauntk.jpg';
+import sea from '../Images/seefood2.jpg';
+import wine from '../Images/wine6.jpg';
+import dessert from '../Images/dessertspecial.jpeg';
 
-const  menuData = [
+ const menuData = [
     {
         id:1,
         category:"Chinesefoods",
-        img:{Chinesefoods},
+        image:chinesefoods,
         text: "Enjoy our in house chinese food that was prepared passinately just for you! Welcome again and again ",
     },
      {
          id:2,
          category:"FamilyDinner",
-         img:{FamilyDinner},
+         image:family,
          text:"Come one,come all with friends and family and enjoy a whole buffet just for you.Enjoy!",
      },
      {
          id:3,
          category:"Indianfoods",
-         img:{Indianfoods},
+         image:indian,
          text:"Introducing some scrumptious indian spicy food just for you and you.Place an order!",
      },
      {
          id:4,
          category:"Koreanfoods",
-         img:{Koreanfoods},
+         image:korean,
          text:"We have currently added some korean food onto our menu after lots of customer demands. Place your order now for a great experience.",
      },
      {
         id:5,
         category:"Mexicanfoods",
-        img:{Mexicanfoods},
+        image:mexican,
         text:"Best made mexican tacos that will leave you with the best tacos experience that you have never had.",
      },
      {
          id:6,
          category:"LaCafe Special",
-         img:{Special},
+         image:special,
          text:"Special meal is our all time cafe recommendation. Come one come all and enjoy our in house special.",
     },
     {
         id:7,
         category:"Seafoods",
-        img:{Sea},
+        image:sea,
         text:"You will definitely enjoy our diversified sea food that we prepare with our secret ingridients",
     },
     {
         id:8,
         category:"Frenchculinary",
-        img:{Wine},
+        image:wine,
         text:"Enjoy our special house recommended wine for going along with the scrumptious meal you just had."
     },
     {
         id:9,
         category:"LaCafe Dessert",
-        img:{Dessert},
+        image:dessert,
         text:"Place dessert order to leave you feeling full and satisfied for the day! ",
     }
 ];
 
-export default menuData;
+ export default menuData;
--- src/Pages/Container.js
+++ src/Pages/Container.js
@@ -3,14 +3,38 @@ import '../App.css';
 import {useState} from 'react';
 import menuData from "../Components/menudata";
 
-
-
 const ButtonContainer = () =>{
-    const [menu,setMenu] = useState(menuData);
+     const [menu,setMenu] = useState([]);
+     const [btn,setBtn] = useState();
+
+    
+     const handleChange =(e) =>{
+         setBtn(e.target.value);
+     
+          setMenu(menuData.map((item) =>item))
+      }
+    //  console.log(menuData);
     
 
 return(
     <>
+    <div className="header">
+      <h3>LaCafe Menu</h3>
+    </div>
+    <div className="section-menu">
+ { btn || menu.map(item =>(
+     <article key = {item.id}>
+     <img 
+     src={item.image}
+     alt={item.category}
+     />
+     <p>{item.text}</p>
+     </article>
+ ))}
+
+        </div>
+        
+            <button className="button-container" type="button" onClick={handleChange}>Open</button>
 
     </>
 )
